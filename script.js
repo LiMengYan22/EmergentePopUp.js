04f032013e79ee05907fda4b9c8704bd357e3094
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-22 11:28:45 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//<head>
+//<script> 
+   function abrir(url) { 
+     open(url,'','top=300, left=300, width=300, height=300') ; 
+     } 
+//</script>
+//</head>
+
+
+//El enlace al HTML:
+//<a href="javascript:abrir('/ejemplo/bancaonline/cuentas.php')">Link</a>
